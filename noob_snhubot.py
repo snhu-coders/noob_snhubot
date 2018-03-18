e0fe73074d79f4b7f5a696280b27741ffc6c8c6e
gsfellis <gregfellis@gmail.com>
2018-03-18 10:02:28 -0400

    Added help functionality

--- noob_snhubot.py
+++ noob_snhubot.py
@@ -70,54 +70,58 @@ def handle_command(command, channel, user):
         ROLL_REGEX = "^([1-9][0-9]{0,2})d([1-9][0-9]{0,2})(([+.-])(\d+))?$"
         roll = command.split()
 
-        response = "That is an invalid roll. Try again"
+        response = "That roll is not valid. Try `@Noob SNHUbot roll help`"   
 
         if len(roll) > 1:
             roll = command.split()[1]
-            match = re.match(ROLL_REGEX, roll)
+            
+            if roll.lower().startswith("help"):
+                response = "Let me show you how I roll:\n`roll XdY[±Z]` rolls X number of Y-Sided dice with an optional positive or negative Z modifier.\nExamples:\n`@Noob SNHUbot roll 2d6`\n`@Noob SNHUbot roll 1d10+3`\n`@Noob SNHUbot roll 20d4-5`"
+            else:
+                match = re.match(ROLL_REGEX, roll)
 
-            if match is not None:
-                rolls = []
-                num_dice = int(match.groups()[0])
-                pips = int(match.groups()[1])            
+                if match is not None:
+                    rolls = []
+                    num_dice = int(match.groups()[0])
+                    pips = int(match.groups()[1])            
 
-                for x in range(num_dice):
-                    rolls.append(random.randint(1, pips))
+                    for x in range(num_dice):
+                        rolls.append(random.randint(1, pips))
 
-                total = sum(rolls)
-                
-                if match.groups()[2] is not None:
-                    mod = int(match.groups()[4])
+                    total = sum(rolls)
+                    
+                    if match.groups()[2] is not None:
+                        mod = int(match.groups()[4])
 
-                    if match.groups()[3] == "+":
-                        total += mod
-                    else:
-                        total -= mod            
-                
-                attach = True
-                attachment = json.dumps([
-                    {
-                        "text":"<@{}> rolled *{}*".format(user, total),
-                        "fields":[
-                            {
-                                "title":"Roll",
-                                "value":"{}".format(match.group()),
-                                "short":"true"
-                            },
-                            {
-                                "title":"Values",
-                                "value":"{}".format(" ".join(str(roll) for roll in rolls)),
-                                "short":"true"
-                            },
-                            {
-                                "title":"Modifier",
-                                "value":"{}".format(match.groups()[2]),
-                                "short":"true"
-                            }
-                        ],
-                        "color":"good"
-                    }
-                ])
+                        if match.groups()[3] == "+":
+                            total += mod
+                        else:
+                            total -= mod            
+                    
+                    attach = True
+                    attachment = json.dumps([
+                        {
+                            "text":"<@{}> rolled *{}*".format(user, total),
+                            "fields":[
+                                {
+                                    "title":"Roll",
+                                    "value":"{}".format(match.group()),
+                                    "short":"true"
+                                },
+                                {
+                                    "title":"Values",
+                                    "value":"{}".format(" ".join(str(roll) for roll in rolls)),
+                                    "short":"true"
+                                },
+                                {
+                                    "title":"Modifier",
+                                    "value":"{}".format(match.groups()[2]),
+                                    "short":"true"
+                                }
+                            ],
+                            "color":"good"
+                        }
+                    ])            
     
     # Sends the response back to the channel
     if attach:
