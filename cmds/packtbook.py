606e31fc6548ddd83acd23f4381da4f98f1a0c20
gsfellis <gregfellis@gmail.com>
2018-05-10 21:39:58 -0400

    Added time stamp calculations and updated output formatting.

--- cmds/packtbook.py
+++ cmds/packtbook.py
@@ -1,34 +1,69 @@
-import re
+import datetime
 import json
-from urllib.request import urlopen
+import time
+
 from bs4 import BeautifulSoup
+from dateutil.relativedelta import relativedelta
+from urllib.request import urlopen
 
 command = 'packtbook'
 
 def execute(command, user):
     response = None
     attachment = None
+    mini = False
+
+    # Optional mini output
+    if len(command.split()) > 1:
+        arg = command.split()[1]
+
+        if arg.lower() == "mini":
+            mini = True
     
     url = 'https://www.packtpub.com/packt/offers/free-learning/' 
     soup = BeautifulSoup(urlopen(url), 'html.parser')
     
+    # Grab the book title
     book_box = soup.find('div', attrs={'class':'dotd-title'})
     book_title = book_box.text.strip()
 
+    # Grab the book image
     book_img = soup.find('img', attrs={'class':'bookimage'})
     book_img_src = book_img['src'].strip().replace(' ', '%20')
 
-    attachment = json.dumps([
+    # Grab the timestamp
+    book_expires = soup.find('span', attrs={'class':'packt-js-countdown'})
+    expires_time = datetime.datetime.fromtimestamp(int(book_expires['data-countdown-to']))
+    cur_time = datetime.datetime.fromtimestamp(int(time.time()))
+    time_diff = expires_time - cur_time
+
+    time_string = "time"
+    
+    # Figure the time out
+    if str(time_diff).count(":") == 2:
+        h, m, s = str(time_diff).split(":")
+
+        time_string = "{} hours, {} minutes, and {} seconds".format(h, m, s)
+
+    if mini:
+        attachment = json.dumps([
         {
-            "title":"Packt Free Book of the Day!",
+            "pretext":"The Packt Free Book of the Day is:",
+            "title":book_title,
             "title_link":url,
-			"text":book_title,
-			"image_url":"https:{}".format(book_img_src),
+			"footer":"There's still {} to get this book!".format(time_string),
+			"thumb_url":"https:{}".format(book_img_src),
             "color":"#ffca5b"
         }])
+    else:
+        attachment = json.dumps([
+            {
+                "pretext":"The Packt Free Book of the Day is:",
+                "title":book_title,
+                "title_link":url,
+                "footer":"There's still {} to get this book!".format(time_string),
+                "image_url":"https:{}".format(book_img_src),
+                "color":"#ffca5b"
+            }])
 
-
-    #print(book_title)
-    #print(book_img_src)
-    #print(attachment)
     return response, attachment
