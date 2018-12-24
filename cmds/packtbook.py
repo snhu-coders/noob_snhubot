8d71e3639cc10e65f817057d5937ac07099c16ad
J.D. Thomason <joseph.thomason@snhu.edu>
2018-12-24 08:55:21 -0700

    Assuming no book sale with empty book title.

--- cmds/packtbook.py
+++ cmds/packtbook.py
@@ -30,45 +30,48 @@ def execute(command, user):
         book_box = soup.find('div', attrs={'class':'dotd-title'})
         book_title = book_box.text.strip()
 
-        # Grab the book image
-        book_img = soup.find('img', attrs={'class':'bookimage'})
-        book_img_src = book_img['src'].strip().replace(' ', '%20')
-        # format book image
-        if book_img_src.lower().startswith("//"):
-            book_img_src = "https" + book_img_src
-        elif not book_img_src.lower().startswith("https://"):
-            book_img_src = "https://" + book_img_src
+        if book_title != "":
+            # Grab the book image
+            book_img = soup.find('img', attrs={'class':'bookimage'})
+            book_img_src = book_img['src'].strip().replace(' ', '%20')
+            # format book image
+            if book_img_src.lower().startswith("//"):
+                book_img_src = "https" + book_img_src
+            elif not book_img_src.lower().startswith("https://"):
+                book_img_src = "https://" + book_img_src
 
-        # Grab the timestamps
-        book_expires = soup.find('span', attrs={'class':'packt-js-countdown'})
-        expires_time = datetime.datetime.fromtimestamp(int(book_expires['data-countdown-to']))
-        cur_time = datetime.datetime.fromtimestamp(int(time.time()))        
-        
-        # Figure out time output (handles plurals)
-        attrs = ['hours', 'minutes', 'seconds']
-        human_readable = lambda delta: ['{} {}'.format(
-            getattr(delta, attr),
-            getattr(delta, attr) > 1 and attr or attr[:-1])
-            for attr in attrs if getattr(delta, attr)]
-        
-        time_diff = relativedelta(expires_time, cur_time)
-        times = human_readable(time_diff)
+            # Grab the timestamps
+            book_expires = soup.find('span', attrs={'class':'packt-js-countdown'})
+            expires_time = datetime.datetime.fromtimestamp(int(book_expires['data-countdown-to']))
+            cur_time = datetime.datetime.fromtimestamp(int(time.time()))
 
-        time_string = ", ".join(times)
-        #time_string = {}, {}, and {}".format(*times)
+            # Figure out time output (handles plurals)
+            attrs = ['hours', 'minutes', 'seconds']
+            human_readable = lambda delta: ['{} {}'.format(
+                getattr(delta, attr),
+                getattr(delta, attr) > 1 and attr or attr[:-1])
+                for attr in attrs if getattr(delta, attr)]
 
-        output = {"pretext":"The Packt Free Book of the Day is:",
-                "title":book_title,
-                "title_link":url,
-                "footer":"There's still {} to get this book!".format(time_string),                
-                "color":"#ffca5b"}
-        
-        if mini:
-            output['thumb_url'] = "{}".format(book_img_src)
+            time_diff = relativedelta(expires_time, cur_time)
+            times = human_readable(time_diff)
+
+            time_string = ", ".join(times)
+            #time_string = {}, {}, and {}".format(*times)
+
+            output = {"pretext":"The Packt Free Book of the Day is:",
+                    "title":book_title,
+                    "title_link":url,
+                    "footer":"There's still {} to get this book!".format(time_string),
+                    "color":"#ffca5b"}
+
+            if mini:
+                output['thumb_url'] = "{}".format(book_img_src)
+            else:
+                output['image_url'] = "{}".format(book_img_src)
+
+            attachment = json.dumps([output])
         else:
-            output['image_url'] = "{}".format(book_img_src)
-
-        attachment = json.dumps([output])
+            response = "Apologies, but there appears to not be a free book today.  Maybe tomorrow! :crossed_fingers:"
 
     except Exception as err:
         print(err)
