a17318c294134c7b5237cbd9e90623a42f4ec8c7
gsfellis <gsfellis@users.noreply.github.com>
2019-07-27 22:23:21 -0400

    Noob snhubot 2.0 (#54)
    * Major Overhaul
    - Separated script, bot, mongo, and slack tasks
    - Eliminated unnecessary circular imports
    - Mongo still needs work
    - Modified all commands with bot as third parameter (bot passes self)
    - Added some stuff to BotHelper
    - Removed homegrown HashTable implementation for Catalog (needs testing)
    
    * Update gitignore and readme
    
    * Removed "junk" folder
    
    * Fix packtbook schedule time in primary script
    
    * Add MongoConn subclass of MongoConnection
    Cleaned up Catalog
    Replaced HashTable with dictionary
    Add connection logging to primary script loop
    Updated BotHelp init for MongoConn
    
    * Classify Update
    Remove HashItem.py and HashTable.py from BotHelper
    Update some PEP8 issues (mostly spacing)
    Added Google style docstrings to methods
    Added Bot cleanup logic (to address hung chrome procs)
    
    * Add and Execute Tests with pytest
    - Add a couple simple tests and infrastructure
    - Fix packtbook deprecation warning for ChromeDriver options
    - Fix config deprecation warning by defining a Loader= for yaml
    
    * Add new tests
    Added cmds module to tests/unit and moved command tests
    Added airspeed_velocity and channels tests
    Removed useless channels processing from greet_user.py
    
    * Updated readme and gitignore
    
    * Add JD's suggestion of "unfurl_links" to new SlackConn class
    
    * Add greet_user test
    Found minor bug in greet_user.py not returning appropriate response if the IM channel does not open properly
    
    * Update greet_user test
    
    * Add it140 test
    
    * Add roll tests
    Updated roll to pass tests and removed erroneous "." modifier symbol
    
    * Correct unicode crash during channels command
    Cleaned up channels command commented code
    
    * Scheduler overhaul
    - Added config-based task scheduling
    - Tasks will only be scheduled when the scheduled time is within the schedule_delay period (600 seconds)
    - Task cleanup will only query the thread.is_active() after the scheduled time has passed
    - Corrected task number output by calling len of schedule
    - Added cfg directory to .gitignore
    - Added command line arguments and logic to primary script for the scheduler config
    - Added Google-style comments to Scheduler.py
    
    * Update README and requirements
    
    * Add email alerting to primary script on crash exception
    
    * Update packtbook command for the 2.0 execute requirements.
    
    * Add .travis.yml for Travis-CI integration
    
    * Add chrome addon to Travis-CI
    
    * Get chromedriver to start when called from Travis
    
    * Correct install section of travis config
    
    * One more time
    
    * Last try tonight
    
    * Add Travis Badge after successfully getting it setup!
    
    * Trying a code formatter

---
+++ BotHelper/Output.py
@@ -0,0 +1,8 @@
+import datetime
+
+
+def output(message):
+    """
+    Print message with UTC timestamp
+    """
+    print(f"UTC: {datetime.datetime.utcnow().timestamp()} - {message}")
--- cmds/__init__.py
+++ cmds/__init__.py
@@ -8,14 +8,12 @@ __all__ = []
 # Dynamically load all submodules in package
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     __all__.append(module_name)
-    module = loader.find_module(module_name).load_module(module_name)    
+    module = loader.find_module(module_name).load_module(module_name)
     exec('%s = module' % module_name)
 
     COMMANDS_ALL[module_name] = module.command
-    
+
     if module.public:
         COMMANDS[module_name] = module.command
     else:
-        COMMANDS_HIDDEN[module_name] = module.command    
-    
-    
+        COMMANDS_HIDDEN[module_name] = module.command
